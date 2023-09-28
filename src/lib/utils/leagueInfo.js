1719b25f0cfb49401d5154483dc73d30468d32a3
Joshua Khani <15787802+jkhani@users.noreply.github.com>
2023-09-28 12:12:23 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -2,17 +2,40 @@
 export const leagueID = "919415855291715584"; // your league ID
 export const leagueName = "Work from Homies"; // your league name
 export const dues = 100; // (optional) used in template constitution page
-export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
+export const dynasty = false; // true for dynasty leagues, false for redraft and keeper
 export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
 
 /*   STEP 2   */
 export const homepageText = `
-  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-  <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-  <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-  <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  <p><b>Established in 2019, Crafting Gridiron Legends Since Day One</b></p>
+  <p>In the world of fantasy football, where gridiron dreams are woven, "Work from Homies" stands as a testament to camaraderie and competition. 
+  Founded in 2019, our league has seen it all – from the exhilarating triumphs to the agonizing defeats, and everything in between.</p>
+  <p><b>Redraft League to Keeper League: The Evolution Begins</p></b>
+  <p>For the past three seasons, we've been a redraft league, where every year is a fresh start. However, the winds of change are upon us. 
+  While the whispers of becoming a keeper league grew louder, it was not to be for 2023. Some passionate managers' voices carried the day, and so, 
+  the journey to our first keeper league season begins in 2024.</p>
+  <p><b>The Trophy: Where Legends Are Etched in Silver</p></b>
+  <p>Our coveted league trophy tells the tale of champions past and present. "imakhanman" etched his name three times as the champion in 2020, 2021, 
+  and shared glory with "rastafasta" in 2022. Not content with just a trophy, "rastafasta" commemorated his victory with a champion's ring. But it's 
+  not just about the bling; it's about the bragging rights that last for a year, the prize pot that awaits the victor, and the sweet taste of success.</p>
+  <p><b>Competition with a Twist: Toilet Bowl or Triumph</p></b>
+  <p>While the champions soar, someone must bear the weight of ignominy. The "Toilet Bowl" is a battle of wits and misfortune, where the worst-performing 
+  manager faces excruciating humiliation. It's a reminder that in fantasy football, every decision counts.</p>
+  <p><b>Strategies and Traditions: Tales of Drafts and Dynasties</p></b>
+  <p>In our league, strategy is king. "JulesOZ" has made it a tradition to secure Travis Kelce, demonstrating that some strategies stand the test of time.</p>
+  <p><b>Generational Bonds: Passing the Torch</p></b>
+  <p>Our league is not just about football; it's about family. Daddies make sure the young'uns watch the games for good luck every week, passing on the torch of tradition and fanaticism.</p>
+  <p><b>Ever-Changing Dynamics: From Dues to Delight</p></b>
+  <p>Over the years, our league has evolved. Dues increased, "SmokeytheCalBear" took over for "scottdelly" in 2021, "mobossmed" plays spoiler, and the unforgettable
+  2022 season ended in an unprecedented manner, thanks to an act of God and split votes.</p>
+  <p><b>Strength in Diversity: From Casual to Die-Hard</p></b>
+  <p>Half the league watches every game, dissecting every play. The other half enjoys a more casual approach, but when it comes to fantasy, they bring their A-game.</p>
+  <p><b>The Ultimate Prizes: Money, Glory, and Shame</p></b>
+  <p>While the ultimate prize is the glory of victory, the spoils of war include a prize pot of $400 for 1st place, $100 for 2nd place, and $50 for 3rd place. 
+  The winner carries the league's bragging rights for a year, while the loser of the Toilet Bowl faces the sting of humiliation.</p>
+  <p>Here at "Work from Homies," we're not just a fantasy football league; we're a community bound by the love of the game, the thrill of competition, and 
+  the pursuit of gridiron glory. So, buckle up, because in this league, every touchdown, fumble, and trade can change your destiny.</p>
+  <p><b>May your fantasy dreams become a reality, and may the best manager win!</p></b>
 `;
 
 /*   STEP 3   */
